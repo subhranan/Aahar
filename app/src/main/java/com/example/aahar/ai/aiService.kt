908fd6e8865b94ad515d32a6fc012ccb7e186f38
Subhranan <>
2025-09-21 23:22:20 +0530

    Update prompt

--- app/src/main/java/com/example/aahar/ai/aiService.kt
+++ app/src/main/java/com/example/aahar/ai/aiService.kt
@@ -13,7 +13,10 @@ class aiService {
     Suggest 6 different meal ideas I can cook using $ingredients.
     Return the output **only as a list of dish names and short descriptions**.
     Do NOT include any introductory sentence like "Here are some…" or "You can try…".
-    Use the format: *Dish Name* Description
+    Use the format: *Dish Name* Description(with exactly one space after the dish name and no 
+    leading spaces before the description). If the input contains any irrelevant, 
+    offensive, filthy, or inappropriate words, ignore them and instead treat them as “fruit” before
+     generating the meals. Only return safe, food-related results.
 """.trimIndent()
         val response = model.generateContent(prompt)
         Log.d("GEMINI", "Response is: ${response.text}")
